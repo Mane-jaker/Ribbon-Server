9e4613933373fb30d7a7e29aea8b8acd1a3d773f
Jorge Brandon Chandomi Esponda <sr.conejo.uwu@gmail.com>
2021-08-27 21:54:49 -0500

    Avances de conejo uwu

---
+++ Ribbon-server/Ribbon-server/Datos de control.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ribbon_server
+{
+    class Datos_de_control
+    {
+
+        public static bool Default = true;
+        public static bool VOpcionesIsActive = false;
+    }
+}
---
+++ Ribbon-server/Ribbon-server/Principal.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ribbon_server
+{
+    /// <summary>
+    /// Lógica de interacción para Principal.xaml
+    /// </summary>
+    public partial class Principal : Window
+    {
+        //Intancias
+        Opciones opcioness = new Opciones();
+
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Opciones_Click(object sender, RoutedEventArgs e)
+        {
+            if(Datos_de_control.VOpcionesIsActive == false)
+            {
+                this.Hide();
+                opcioness.Show();
+                Datos_de_control.VOpcionesIsActive = true;
+            }
+            else if(Datos_de_control.VOpcionesIsActive == true)
+            {
+               this.Hide();
+               opcioness.Visibility = Visibility.Visible;
+            }
+         
+        }
+
+        public static void Visibilidad()
+        {
+           
+        }
+
+    }
+}
